5c01f5ccb4aed82b49b033f468ec74bb87f8f2fe
Vincent <extras.hrml@gmail.com>
2017-04-15 13:10:44 +0200

    Ajout des pions à la classe joueur

--- src/main/java/modeles/Joueur.java
+++ src/main/java/modeles/Joueur.java
@@ -6,11 +6,28 @@ public class Joueur {
 
 	//carte de destinations ou itineraire que possede le joueur concerne
 	private ArrayList<CarteDestination> cartesDestination = new ArrayList<CarteDestination>();
+
+	//Pions en possession du joueur
+	private int nbPionsBateau;
+	private int nbPionsBateauReserve;
+	private int nbPionsWagons;
+	private int nbPionsWagonsReserve;
+
 	
 	//cartes transports que possede le joueur concerne
 	private ArrayList<CarteTransport> cartesTransport = new ArrayList<CarteTransport>();
 
 	private String couleur = "pink";
+
+	public Joueur(ArrayList<CarteDestination> cartesDestination, int nbPionsBateau, int nbPionsBateauReserve, int nbPionsWagons, int nbPionsWagonsReserve, ArrayList<CarteTransport> cartesTransport, String couleur) {
+		this.cartesDestination = cartesDestination;
+		this.nbPionsBateau = 20;
+		this.nbPionsBateauReserve = 5;
+		this.nbPionsWagons = 40;
+		this.nbPionsWagonsReserve = 10;
+		this.cartesTransport = cartesTransport;
+		this.couleur = couleur;
+	}
 
 	public Joueur(ArrayList<CarteDestination> cartesDestination, ArrayList<CarteTransport> cartesTransport) {
 		super();
@@ -20,6 +37,38 @@ public class Joueur {
 
 	public Joueur(){
 
+	}
+
+	public int getNbPionsBateau() {
+		return nbPionsBateau;
+	}
+
+	public void setNbPionsBateau(int nbPionsBateau) {
+		this.nbPionsBateau = nbPionsBateau;
+	}
+
+	public int getNbPionsBateauReserve() {
+		return nbPionsBateauReserve;
+	}
+
+	public void setNbPionsBateauReserve(int nbPionsBateauReserve) {
+		this.nbPionsBateauReserve = nbPionsBateauReserve;
+	}
+
+	public int getNbPionsWagons() {
+		return nbPionsWagons;
+	}
+
+	public void setNbPionsWagons(int nbPionsWagons) {
+		this.nbPionsWagons = nbPionsWagons;
+	}
+
+	public int getNbPionsWagonsReserve() {
+		return nbPionsWagonsReserve;
+	}
+
+	public void setNbPionsWagonsReserve(int nbPionsWagonsReserve) {
+		this.nbPionsWagonsReserve = nbPionsWagonsReserve;
 	}
 
 	public ArrayList<CarteDestination> getCartesDestination() {
