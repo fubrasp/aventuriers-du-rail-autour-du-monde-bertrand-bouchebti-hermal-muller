dbf4128abbcd28efe10ce3cc8bc239e783effd7a
Vincent <extras.hrml@gmail.com>
2017-04-26 17:34:02 +0200

    Suppression variable inutile test

--- src/test/java/ScoreTest.java
+++ src/test/java/ScoreTest.java
@@ -15,8 +15,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JUnitPlatform.class)
 public class ScoreTest {
-
-    Jeu jeu;
+    
     Joueur joueur;
 
     public static int randomInt(int min, int max) {
